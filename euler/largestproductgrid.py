ac01fc53410625aad4206f879ed0371e3452681e
Steven Tompkins <stompkins@shelterinsurance.com>
2016-12-29 16:20:04 -0600

    Finished problem 11 solution

--- euler/largestproductgrid.py
+++ euler/largestproductgrid.py
@@ -2,6 +2,8 @@ from util import timing
 from pkg_resources import resource_filename, resource_listdir
 import os
 
+# Project Euler problem 11 solution
+# Result: 70600674, pass
 
 def load_data_for_grid():
     global grid
@@ -24,25 +26,60 @@ def load_data_for_grid():
 def main():
     load_data_for_grid()
     print("grid loaded")
-    right_product = 0
-    left_product = 0
-    up_product = 0
-    down_product = 0
+    right_left_product = 0
+    up_down_product = 0
     diag_product = 0
 
     for i in range(0, len(grid)):
         for j in range(0, len(grid[0])):
             # go right/left
+            if j + 3 < len(grid[0]):
+                numbers = [grid[i][j], grid[i][j+1], grid[i][j+2], grid[i][j+3]]
+                prod = calculate_largest(numbers)
+
+                if right_left_product == 0: right_left_product = prod
+                else:
+                    if prod > right_left_product: right_left_product = prod
+                
+            
+            # go up/down
             if i + 3 < len(grid):
                 numbers = [grid[i][j], grid[i+1][j], grid[i+2][j], grid[i+3][j]]
-                prod = calculate_largest(numbers)
-                if right_product == 0:
-                    right_product = prod
-                else:
-                    if prod > right_product:
-                        right_product = prod
-                        
+                prod = calculate_largest(numbers)    
 
+                if up_down_product == 0: up_down_product = prod
+                else:
+                    if prod > up_down_product: up_down_product = prod
+
+            # go diagonal
+            if (i + 3 < len(grid)) and (j + 3 < len(grid[0])):
+                numbers = [grid[i][j], grid[i+1][j+1], grid[i+2][j+2], grid[i+3][j+3]]
+                prod = calculate_largest(numbers)
+
+                if diag_product == 0: diag_product = prod
+                else:
+                    if prod > diag_product: diag_product = prod
+
+            if (i - 3 > 0) and (j + 3 < len(grid[0])):
+                numbers = [grid[i][j], grid[i-1][j+1], grid[i-2][j+2], grid[i-3][j+3]]
+                prod = calculate_largest(numbers)
+
+                if prod > diag_product: diag_product = prod
+
+    print("Left-Right: %d"  % (right_left_product))
+    print("Up-Down: %d"  % (up_down_product))
+    print("Diagonal: %d"  % (diag_product))
+
+    numbers = [right_left_product, up_down_product, diag_product]
+    largest = 0
+
+    for number in numbers:
+        if number == 0: largest = number
+        else:
+            if number > largest: largest = number
+
+
+    print("Largest: %d" % (largest))
 
 
 
@@ -58,7 +95,7 @@ def calculate_largest(numbers):
     product = 1
 
     for number in numbers:
-        product *= number
+        product *= int(number)
 
     return product
 
